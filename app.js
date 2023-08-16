bb9a2c142de857441d0295fc635f1f50e5f4af9b
instructor SENA CBA <instructor.sena.cba@gmail.com>
2023-08-16 15:33:06 -0500

    modifique el menu de bienvenida

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ require('colors');
 const main = async () => {
 
     console.log('hola SENA CBA'.red);
+    console.log('hola SENA CBA'.red);
+    console.log('hola SENA CBA'.red);
+    console.log('hola SENA CBA'.red);
+    console.log('hola SENA CBA'.red);
+    console.log('hola SENA CBA'.red);
+    console.log('hola SENA CBA'.red);
+    console.log('hola SENA CBA'.red);
+    console.log('hola SENA CBA'.red);
+    console.log('hola SENA CBA'.red);
 
 }
 
